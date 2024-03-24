7700a9186232fc9fcc0f17aa5f986a90b73a4c12
aryanpillai103 <113632962+aryanpillai103@users.noreply.github.com>
2024-03-24 12:19:03 +0000

    basics covered

---
+++ 01_basics/variables.js
@@ -0,0 +1,8 @@
+const accountId = 144553
+let accountPassword = "12345"
+let accountEmail = "aryan@google.com"
+
+// accountId=21334 /*this is not allowed because of problem in block scope */
+
+let accountState;
+console.table([accountId,accountPassword,accountEmail,accountState])
